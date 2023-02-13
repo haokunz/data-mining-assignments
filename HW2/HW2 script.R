7a93420fb19956d6291e906999f46c4ad77b7a75
Haokun Zhang <haokun.zhang@utexas.edu>
2023-02-12 19:41:00 -0600

    Create HW2 script.R

---
+++ HW2/HW2 script.R
@@ -0,0 +1,56 @@
+# ECO 395M: Exercises 2
+
+## Saratoga house prices
+
+### write a better lm model
+
+library(tidyverse)
+library(ggplot2)
+library(modelr)
+library(rsample)
+library(mosaic)
+data(SaratogaHouses)
+#SaratogaHouses$livingArea2 <- SaratogaHouses$livingArea^2
+SaratogaHouses$loglivingArea <- log(SaratogaHouses$livingArea)
+#SaratogaHouses$logprice <- log(SaratogaHouses$price)
+
+
+glimpse(SaratogaHouses)
+
+####
+# Compare out-of-sample predictive performance
+####
+
+# Split into training and testing sets
+saratoga_split = initial_split(SaratogaHouses, prop = 0.8)
+saratoga_train = training(saratoga_split)
+saratoga_test = testing(saratoga_split)
+
+# Fit to the training data
+# Sometimes it's easier to name the variables we want to leave out
+# The command below yields exactly the same model.
+# the dot (.) means "all variables not named"
+# the minus (-) means "exclude this variable"
+lm1 = lm(price ~ lotSize + bedrooms + bathrooms, data=saratoga_train)
+lm2 = lm(price ~ . - pctCollege - sewer - waterfront - landValue - newConstruction -loglivingArea, data=saratoga_train)
+lm3 = lm(price ~ (. - pctCollege - sewer - waterfront - landValue - newConstruction)^2, data=saratoga_train)
+
+#hand build a new model
+lm4 = lm(price ~ . - pctCollege - sewer - waterfront - fuel - landValue -livingArea - newConstruction -fireplaces +livingArea:centralAir +bathrooms:heating +heating:fuel, data=saratoga_train)
+
+summary(lm2)
+
+coef(lm1) %>% round(0)
+coef(lm2) %>% round(0)
+coef(lm3) %>% round(0)
+coef(lm4) %>% round(0)
+
+# Predictions out of sample
+# Root mean squared error
+rmse(lm1, saratoga_test)
+rmse(lm2, saratoga_test)
+rmse(lm3, saratoga_test)
+rmse(lm4, saratoga_test)
+
+# Can you hand-build a model that improves on all three?
+# Remember feature engineering, and remember not just to rely on a single train/test split
