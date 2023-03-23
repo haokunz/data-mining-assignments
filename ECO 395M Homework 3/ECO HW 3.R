f134e1c1acd830df02d564c65e3aefb0be62ec0e
Haokun Zhang <haokun.zhang@utexas.edu>
2023-03-23 00:31:53 -0500

    added new code for question 2

--- ECO 395M Homework 3/ECO HW 3.R
+++ ECO 395M Homework 3/ECO HW 3.R
@@ -39,154 +39,158 @@ library(foreach)
 library(gamlr)
 library(modelr)
 
-# this function actually prunes the tree at that level
-prune_1se = function(my_tree) {
-  out = as.data.frame(my_tree$cptable)
-  thresh = min(out$xerror + out$xstd)
-  cp_opt = max(out$CP[out$xerror <= thresh])
-  prune(my_tree, cp=cp_opt)
-}
-# a handy function for picking the smallest tree 
-# whose CV error is within 1 std err of the minimum
-cp_1se = function(my_tree) {
-  out = as.data.frame(my_tree$cptable)
-  thresh = min(out$xerror + out$xstd)
-  cp_opt = max(out$CP[out$xerror <= thresh])
-  cp_opt
-}
+## Exercise 3 2023
+# Question 2 : Tree modeling: dengue cases
 
-# load data
-dengue = read.csv('../data/dengue.csv')
-N = nrow(dengue)
-dengue$total_cases <- as.numeric(as.character(dengue$total_cases))
-dengue$city <- as.factor(dengue$city)
-dengue$season <- as.factor(dengue$season)
+# Our goal for this problem: 
+# 1. Use CART, random forests, 
+# and gradient-boosted trees to predict dengue cases
+# based on the features available in the data set
 
-# split into training and testing
-train_frac = 0.8
-N_train = floor(train_frac*N)
-N_test = N - N_train
-train_ind = sample.int(N, N_train, replace=FALSE) %>% sort
-dengue_train = dengue[train_ind,]
-dengue_test = dengue[-train_ind,]
+# 2. for whichever model has the better performance on the testing data, 
+# make three partial dependence plots: specific_humidity, precipitation_amt
+# and wild card/writer's choice: you choose a feature that looks 
+# interesting and make a partial dependence plot for that.
 
-##
-# build a classification tree with variables indicated in questions
-dengue_tree= rpart(total_cases ~ ., 
-                   data = dengue_train)
 
-# plot the tree
-rpart.plot(dengue_tree, type=4, extra=1)
+## Train of thought
+## 1. Build CART model, Random forest model and Gradient-boosted trees
+## 2. Uses the 3 models to predict the dengue cases( depend on testing set)
+## 3. Make the partial dependence plots 
 
-# the various summaries of the tree
-print(dengue_tree) # the structure
-summary(dengue_tree)  # more detail on the splits
-# cross-validated error plot.
-plotcp(dengue_tree)
-# you could squint at the table...
-printcp(dengue_tree)
-# picking the smallest tree whose CV error is within 1 std err of the minimum
-cp_1se(dengue_tree)
+## Problem 1: How to build CART model?
+## Problem 2: How to build Random Forest model?
+## Problem 3: How to build Gradient-boosted trees?
+## Problem 4: How to make the partial dependence plots?
 
-# in-sample fit, i.e. predict on the original training data
-# this returns predicted class probabilities
-predict(dengue_tree, newdata=dengue_train)
 
-##
-# Regression trees
-##
 
-# grow a smallish tree
-# larger cp and insplit means stop at a smaller tree
-dengue.tree = rpart(total_cases~ ., 
-                    data=dengue_train,
-                    control = rpart.control(cp = 0.0002, minsplit = 10))
-# this says: split only if you have at least 10 obs in a node,
-# and the split improves the fit by a factor of 0.0002
+# Loaded the needed packages
+library(tidyverse)
+library(ggplot2)
+library(rsample)
+library(modelr)
+library(randomForest)
+library(caret)
+library(gbm)
+library(ggmap)
+library(glmnet)
+library(rpart) # a powerful ML library that is used for building classification and regression trees
+library(gamlr)
+library(rpart.plot)
+library(data.table)
+library(DMwR2)
 
-# plot the tree
-# see ?rpart.plot for the various plotting options here (type, extra)
-rpart.plot(dengue.tree, digits=-5, type=4, extra=1)
+# PART 1: Data wrangling
 
-# cross-validated error plot.
-plotcp(dengue.tree)
-# you could squint at the table...
-printcp(dengue.tree)
-# picking the smallest tree whose CV error is within 1 std err of the minimum
-cp_1se(dengue.tree)
+# Read the dataset
+dengue_fever <- read.csv("https://raw.githubusercontent.com/jgscott/ECO395M/master/data/dengue.csv")
+head(dengue_fever)
 
-# let's prune our tree at the 1se complexity level
-dengue.tree_prune = prune_1se(dengue.tree)
-#plot the tree 
-rpart.plot(dengue.tree_prune, type=4, digits=-5, extra=1, cex=0.5)
+# Use KNN to impute the n.a. value
+dengue_fever$total_cases <- as.numeric(as.character(dengue_fever$total_cases))
+dengue_fever$city <- as.factor(dengue_fever$city)
+dengue_fever$season <- as.factor(dengue_fever$season)
+imputeDengue <- knnImputation(dengue_fever, k = 10, scale = T, meth = "median", distData = NULL)
+head(imputeDengue)
 
-##
-# random forests
-##
+# Split the dataset into training set and testing set
+imputeDengue = initial_split(imputeDengue, prop = 0.8) # 80% of the data as training data
+dengue_training = training(imputeDengue)
+dengue_testing = testing(imputeDengue)
 
-dengue_forest = randomForest(total_cases ~ .,
-                       data = dengue_train, 
-                       na.action = na.roughfix, importance=TRUE)
-# shows out-of-bag MSE as a function of the number of trees used
-plot(dengue_forest)
+# PART 2: Model building 
+
+# CART model building
+cart_dengue = rpart(total_cases ~ . , data = dengue_training, 
+                    control = rpart.control(cp = 0.002, minsplit=20))
+### Split only if we have at least 20 obs in a node,
+### and the split improves the fit by a factor of 0.002 aka 0.2%
+
+# Random Forest model building
+## in the "random_forest_example.R"
+rforest_dengue = randomForest(total_cases ~ .,
+                              data = dengue_training, 
+                              importance=TRUE)
+
+# Gradient-boosted model building
+## in the "capmetro.R"
+gbm_dengue = gbm(total_cases ~ .,
+                 data = dengue_training, 
+                 interaction.depth=4, n.trees=500, shrinkage=.05)
+
+# PART 3: Use the models to Predict the infection cases, and find the model has the best performance
+# Predict the dengue cases with CART model, Random Forest model and Gradient-boosted model
+yhat_cart = predict(cart_dengue, dengue_testing)
+plot(yhat_cart, dengue_testing$total_cases)
+rmse(cart_dengue, dengue_testing)
+
+yhat_rf = predict(rforest_dengue, dengue_testing)
+plot(yhat_rf, dengue_testing$total_cases)
+rmse(rforest_dengue, dengue_testing)
+
+yhat_gbm = predict(gbm_dengue, dengue_testing, n.trees=350)
+plot(yhat_gbm, dengue_testing$total_cases)
+rmse(gbm_dengue, dengue_testing)
+
 
 # let's compare RMSE on the test set
-modelr::rmse(dengue_tree, dengue_test)
-modelr::rmse(dengue.tree, dengue_test)
-modelr::rmse(dengue_forest, dengue_test) # randomforest has less RMSE
-
-yhat_dengue2 = predict(dengue_forest, dengue_test)
-rmse_dengue2 = mean((yhat_dengue2 - dengue_test$total_cases)^2) %>% sqrt
-
-##
-# boosting
-##
-
-# boost model with default distribution
-dengue_boost = gbm(total_cases ~ .,
-                   data = dengue_train, 
-                   interaction.depth=4, n.trees=500, shrinkage=.05)
-
-# Look at error curve
-gbm.perf(dengue_boost)
-
-yhat_test_gbm = predict(dengue_boost, dengue_test, n.trees=500)
-
-# RMSE
-modelr::rmse(dengue_boost, dengue_test)
+modelr::rmse(cart_dengue, dengue_testing)
+modelr::rmse(rforest_dengue, dengue_testing) 
+modelr::rmse(gbm_dengue, dengue_testing) 
 
 
-# What if we assume a Poisson error model?
-dengue_boost2 = gbm(total_cases ~ ., 
-             data = dengue_train, distribution='poisson',
-             interaction.depth=4, n.trees=500, shrinkage=.05)
+# evaluate the performance of three models by k-folds
+# Perform cross-validation
+set.seed(123)
+k <- 5
+cart_cv <- rpart.control(cp = 0.01)
+rf_cv <- list(mtry = sqrt(ncol(dengue_training)), replace = TRUE)
+gb_cv <- list(n.trees = 1000, interaction.depth = 4, shrinkage = 0.01, cv.folds = k)
 
-# error curve
-gbm.perf(dengue_boost2)
+cart_cv_results <- rpart(total_cases ~ ., data = dengue_training, control = cart_cv)
+rf_cv_results <- randomForest(total_cases ~ ., data = dengue_training, mtry = rf_cv$mtry, replace = rf_cv$replace)
+gb_cv_results <- gbm(total_cases ~ ., data = dengue_training, n.trees = gb_cv$n.trees, interaction.depth = gb_cv$interaction.depth, shrinkage = gb_cv$shrinkage, cv.folds = gb_cv$cv.folds, verbose = FALSE)
 
-# Note: the predictions for a Poisson model are on the log scale by default
-# use type='response' to get predictions on the original scale
-# all this is in the documentation, ?gbm
-yhat_test_gbm2 = predict(dengue_boost2, dengue_test, n.trees=500, type='response')
+# Evaluate the performance of each model
+cart_performance <- predict(cart_cv_results, newdata = dengue_testing)
+rf_performance <- predict(rf_cv_results, newdata = dengue_testing)
+gb_performance <- predict(gb_cv_results, newdata = dengue_testing, n.trees = gb_cv$n.trees)
 
-# but this subtly messes up the rmse function, which uses predict with default args
-# so we need to roll our own calculate for RMSE
-(yhat_test_gbm2 - dengue_test$total_cases)^2 %>% mean %>% sqrt
+# Compare the performance of each model by measuring MSE
+cart_accuracy <- mean((cart_performance - dengue_testing$total_cases)^2)
+rf_accuracy <- mean((rf_performance - dengue_testing$total_cases)^2)
+gb_accuracy <- mean((gb_performance - dengue_testing$total_cases)^2)
+
+# print out the MSE of each model to console, the lower the better the model is 
+cat("CART accuracy:", cart_accuracy, "\n")
+cat("Random Forest accuracy:", rf_accuracy, "\n")
+cat("Gradient Boosting accuracy:", gb_accuracy, "\n") # gradient boosting has the lowest MSE
+
+# Because the gradient-boosted model has the smallest out-of-sample RMSE and MSE,
+# we decided to choose it as the model to make partial dependence plots
+
+# PART 4: Partial dependence plots
+# Make the partial dependence plots
+## gbm partial plots are in the "capmetro.R"
+##### partialPlot(gbm_dengue, dengue_testing, 'specific_humidity', las=1)
+
+plot(gbm_dengue, 'specific_humidity', main = "Partial Dependence on specific humidity")
+plot(gbm_dengue, 'precipitation_amt', main = "Partial Dependence on precipitation_amt")
+plot(gbm_dengue, 'tdtr_k', main = "Partial Dependence on tdtr_k")
+# We choose "tdtr_k" to make a partial dependence plots because we think that 
+# if the DTR is bigger, it's more difficult for mosquito to live
+# as a result, we want to know whether DTR affect the total dengue fever cases
+# and we can see that it has big influence on the infection cases.
+
+## Problem: y是total_cases嗎？
+
+####p1 = pdp::partial(gbm_dengue, pred.var = 'specific_humidity', n.trees=350)
+####p1
+####ggplot(p1) + geom_point(mapping=aes(x=specific_humidity, y=total_cases)) 
 
 
-# What if we assume a gaussian error model?
-dengue_boost3 = gbm(total_cases ~ ., 
-                    data = dengue_train, distribution='gaussian', # guassian for std error
-                    interaction.depth=4, n.trees=500, shrinkage=.05)
-
-# error curve
-gbm.perf(dengue_boost3)
-
-# predict yhat
-yhat_test_gbm = predict(dengue_boost3, dengue_test, n.trees=500)
-
-# RMSE
-modelr::rmse(dengue_boost3, dengue_test)
+## Problem: cross-validation??
 
 
 ## Question 3: Predictive model building: green certification
